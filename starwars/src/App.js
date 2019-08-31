faef628d8586357ec557fb845958c17cd5a0df17
RobertoRamirez0 <ramirez.robertomu22@gmail.com>
2019-08-31 15:01:09 -0400

    mvp complete

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 import Axios from 'axios';
+import styled from "styled-components";
 import CharacterInfo from "./components/CharacterInfo"
 
 const App = () => {
@@ -18,16 +19,20 @@ const App = () => {
     })
   }, []);
 
-  console.log(info);
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  const AppDiv = styled.div `
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    align-content: space-around;
+  `
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
-      {info.map((e, index)=>{
+      <AppDiv >
+        {info.map((e, index)=>{
         return <CharacterInfo name={e.name} key ={index} gender={e.gender} year={e.birth_year} />
-      })}
+        })}
+      </AppDiv>
     </div>
   );
 }
